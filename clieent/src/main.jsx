f9eaa2c090ae125fceb15a55ceb75d46bbf1d27a
Mourad <test@example.com>
2026-08-24 20:21:43 +0100

    step1

--- clieent/src/main.jsx
+++ clieent/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home.jsx';
+import Html from './pages/html.jsx';
+import Css from './pages/css.jsx'
+import Javascript from './pages/javascript.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>SORROY.....</h1>
+  },
+
+   {
+    path: "/html",
+    element: <Html/>,
+    
+  },
+   {
+    path: "/css",
+    element: <Css/>,
+    
+  },
+   {
+    path: "/javascript",
+    element: <Javascript/>,
+   },
+
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
